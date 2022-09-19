92ed65c4bb4a0dfdf36a1d854d84dfb40a54d060
xhellcat1942x <75864133+xhellcat1942x@users.noreply.github.com>
2022-09-19 09:56:05 -0600

    cleaned up index
    Added redirect if user not signed in

--- dbs/pages/index.js
+++ dbs/pages/index.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import umbrella from "../public/umbrella.svg";
@@ -7,18 +7,20 @@ import { useRouter } from "next/router";
 import { UserContext } from "../lib/context";
 
 const index = () => {
-  const dict = useContext(UserContext);
+  const context = useContext(UserContext);
+  const router = useRouter();
 
-  const testFunc = () => {
-    console.log("test", dict);
-  };
+  useEffect(() => {
+    let unsubscribe;
+    if (!context.user) {
+      router.replace("/login");
+    }
+  }, [context]);
 
   return (
     <>
       <div className="flex flex-col justify-between h-full bg-[#fff]">
         <div className="max-w-7xl pt-8 h-full mx-auto px-2 sm:px-6 lg:px-8 flex flex-col items-center md:items-start md:flex-row md:justify-between">
-          {/* <div>{dict.user}</div> */}
-          {/* <button onClick={testFunc()}>click</button> */}
           <div className="bg-gray-800 sm:max-w-[50%] px-4 py-[15%]"></div>
           <div className="bg-gray-600"></div>
         </div>
