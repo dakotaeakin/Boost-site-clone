f1356bf95bd8774cb6909dc8c860e260490d1254
xhellcat1942x <75864133+xhellcat1942x@users.noreply.github.com>
2022-11-24 07:43:08 -0700

    Reset project files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Boost-site-clone
- 
--- dbs/components/DashboardFooter.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const DashboardFooter = () => {
-  return <div className="bg-red-300 h-[300px] w-full">DashbaordFooter</div>;
-};
-
-export default DashboardFooter;
