296f0d68a820e2ff1ca575b14571a87ef468a930
xhellcat1942x <75864133+xhellcat1942x@users.noreply.github.com>
2022-12-11 22:51:21 -0700

    Work on settings

--- app/components/dashboard/settings/Settings.jsx
+++ app/components/dashboard/settings/Settings.jsx
@@ -1,7 +1,91 @@
+import { Box, Button, Link, Typography, useMediaQuery } from "@mui/material";
 import React from "react";
 
 const Settings = () => {
-  return <div>Settings</div>;
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+
+  return (
+    <Box height="fit-content" width="100%">
+      {/*Nav*/}
+      <Box m={isNonMobile ? "0 30px 0 30px" : "0 60px 0 30px"} display="flex">
+        <Box borderBottom="solid" borderColor="#f25d12">
+          <Button>My Profile</Button>
+        </Box>
+        <Box borderBottom="solid 2px" borderColor="lightGrey">
+          <Button sx={{ color: "grey" }}>Security</Button>
+        </Box>
+      </Box>
+      {/*Content*/}
+      <Box
+        boxShadow="0 0 5px black"
+        height="fit-content"
+        m={isNonMobile ? "30px 30px 0 30px" : "30px 60px 0 30px"}
+        borderRadius="5px"
+        gridColumn="1 / span 2"
+      >
+        <Box>
+          <Typography>FirstName LastName</Typography>
+          <Box>
+            <Typography>Account Number:</Typography>
+            <Typography>123456789</Typography>
+          </Box>
+        </Box>
+        <Box p="15px">
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            borderBottom="solid 1px"
+            mt="10px"
+          >
+            <Typography>Full Name</Typography>
+            <Box display="flex">
+              <Typography>FirstName LastName</Typography>
+              <Link
+                ml="10px"
+                sx={{
+                  "&.MuiLink-root": {
+                    cursor: "pointer",
+                  },
+                }}
+              >
+                Edit
+              </Link>
+            </Box>
+          </Box>
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            borderBottom="solid 1px"
+            mt="10px"
+          >
+            <Typography>Email</Typography>
+            <Box display="flex">
+              <Typography>blah.com</Typography>
+              <Link
+                ml="10px"
+                sx={{
+                  "&.MuiLink-root": {
+                    cursor: "pointer",
+                  },
+                }}
+              >
+                Edit
+              </Link>
+            </Box>
+          </Box>
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            borderBottom="solid 1px"
+            mt="10px"
+          >
+            <Typography>Address</Typography>
+            <Typography>blah blah</Typography>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Settings;
